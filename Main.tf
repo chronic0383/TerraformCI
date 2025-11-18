f8aa6712e1017145dcfd207c68ad256a4f7a1d35
James Clease <jclease@jclabs.cloud>
2025-11-18 17:20:37 +0000

    route table added

--- Main.tf
+++ Main.tf
@@ -47,8 +47,20 @@ resource "azurerm_virtual_network_peering" "jclabsvnet1-to-vnet2" {
   resource_group_name       = azurerm_resource_group.rg.name
   virtual_network_name      = azurerm_virtual_network.jclabsvnet1.name
   remote_virtual_network_id = "/subscriptions/8b5b8b3f-1b64-4e47-a3cf-0bd762fc97db/resourceGroups/TR-Peering-test/providers/Microsoft.Network/virtualNetworks/jclabsvnet2"
-  allow_forwarded_traffic   = true
+  allow_forwarded_traffic   = false
   allow_gateway_transit     = false
   use_remote_gateways       = false
+}
+
+resource "azurerm_route_table" "jclabsroutetable1" {
+  name                = "jclabsroutetable1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  route {
+    name           = "routeToVnet2"
+    address_prefix = "10.2.0.0/16"
+    next_hop_type  = "VirtualNetworkPeering"
+  }
 }
 # End of Main.tf
