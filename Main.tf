373131bd605adbd15829f81d0003255858c663c0
James Clease <jclease@jclabs.cloud>
2025-11-19 18:40:02 +0000

    expanding terraform

--- Main.tf
+++ Main.tf
@@ -1,25 +1,3 @@
-terraform {
-  required_version = "1.14.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>4.40.0"
-    }
-  }
-
-  cloud {
-
-    organization = "jclabs-learningTF"
-
-    workspaces {
-      name = "remotestate"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
 
 resource "azurerm_resource_group" "rg" {
   name     = "rgterraform"
---
+++ Versions.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = "1.14.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>4.40.0"
+    }
+  }
+
+  cloud {
+
+    organization = "jclabs-learningTF"
+
+    workspaces {
+      name = "remotestate"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
