b1db418a8aa86fbeedf5b6581305c71e26cf6cab
James Clease <jclease@jclabs.cloud>
2025-11-20 16:59:26 +0000

    peering from TF side

--- Main.tf
+++ Main.tf
@@ -30,8 +30,17 @@ resource "azurerm_virtual_network" "vnet1" {
     name             = "Subnet1"
     address_prefixes = ["10.1.1.0/24"]
   }
+}
 
-
+resource "azurerm_virtual_network_peering" "vnet1-to-vnet2" {
+  name                         = "vnet1-to-vnet2"
+  resource_group_name          = azurerm_resource_group.rg.name
+  virtual_network_name         = azurerm_virtual_network.vnet1.name
+  remote_virtual_network_id    = "/subscriptions/8b5b8b3f-1b64-4e47-a3cf-0bd762fc97db/resourceGroups/TR-Peering-test/providers/Microsoft.Network/virtualNetworks/jclabsvnet2"
+  allow_forwarded_traffic      = false
+  allow_gateway_transit        = false
+  use_remote_gateways          = false
+  allow_virtual_network_access = true
 }
 # End of Main
 #
