1e5219cd0954680e4fa04879e9f91b6f24216477
James Clease <jclease@jclabs.cloud>
2025-11-20 16:42:49 +0000

    Vnet creation

--- Main.tf
+++ Main.tf
@@ -19,6 +19,20 @@ resource "azurerm_storage_container" "tfstate" {
   container_access_type = "private"
 
 }
+
+resource "azurerm_virtual_network" "vnet1" {
+  name                = "Vnet1uks"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  subnet {
+    name             = "Subnet1"
+    address_prefixes = "10.1.1.0/24"
+  }
+
+
+}
 # End of Main
 #
 #
