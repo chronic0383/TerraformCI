09371d3bbb4ac7ef1d5339a5a2833295b0e51f9c
James Clease <jclease@jclabs.cloud>
2025-11-20 15:37:58 +0000

    added storage

--- Main.tf
+++ Main.tf
@@ -4,7 +4,14 @@ resource "azurerm_resource_group" "rg" {
   location = var.primary_location
 }
 
+resource "azurerm_storage_account" "storage1" {
+  name                     = "st${var.application_name}${var.environment_name}001"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
+}
 # End of Main
 #
 #
