e1883e37c64022c4c1115041183abaffeb130f3a
James Clease <jclease@jclabs.cloud>
2025-11-21 18:19:22 +0000

    moving storage from main

--- Main.tf
+++ Main.tf
@@ -3,25 +3,6 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.application_name}-${var.environment_name}"
   location = var.primary_location
 }
-
-resource "azurerm_storage_account" "storage1" {
-  name                     = "st${var.application_name}${var.environment_name}001"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-}
-
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.storage1.name
-  container_access_type = "private"
-
-}
-
-
 # End of Main
-#
-#
+
 
