1b376c62e7115e0a25b78978858804536615df80
James Clease <jclease@jclabs.cloud>
2025-11-24 18:52:36 +0000

    Random-string for resource suffix local

--- Main.tf
+++ Main.tf
@@ -4,12 +4,32 @@ resource "azurerm_resource_group" "rg" {
   location = var.primary_location
 }
 resource "azurerm_log_analytics_workspace" "rg" {
-  name                = "example"
+  name                = "jclabsterranaltics"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
+resource "random_string" "resource_suffix" {
+  length  = 6
+  upper   = false
+  number  = true
+  special = false
+}
+
+
+resource "azurerm_key_vault" "main" {
+  name                        = "kv-${var.application_name}-${var.environment_name}-${random_string.resource_suffix.result}"
+  location                    = azurerm_resource_group.rg.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+}
+
 # End of Main
 
 
