003845ee1a7c520cd7167abf3463afa992cb5258
James Clease <jclease@jclabs.cloud>
2025-11-24 12:45:58 +0000

    log analytics

--- Main.tf
+++ Main.tf
@@ -3,6 +3,13 @@ resource "azurerm_resource_group" "rg" {
   name     = "rg-${var.application_name}-${var.environment_name}"
   location = var.primary_location
 }
+resource "azurerm_log_analytics_workspace" "rg" {
+  name                = "example"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+}
 # End of Main
 
 
