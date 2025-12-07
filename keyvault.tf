62102829caa7fa16ab8d1fcd961f4b76f3dce148
James Clease <jclease@jclabs.cloud>
2025-12-07 16:17:54 +0000

    new keyvault soft delete 7

--- keyvault.tf
+++ keyvault.tf
@@ -4,7 +4,7 @@ resource "azurerm_key_vault" "example" {
   resource_group_name         = azurerm_resource_group.rg.name
   enabled_for_disk_encryption = true
   tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 3
+  soft_delete_retention_days  = 7
   purge_protection_enabled    = false
 
   sku_name = "standard"
