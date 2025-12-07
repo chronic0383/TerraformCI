f6f5f33a2bd3d86a7a35696d210be1fc5917b709
James Clease <jclease@jclabs.cloud>
2025-12-07 17:02:08 +0000

    storage account firewall part1

--- Storage.tf
+++ Storage.tf
@@ -5,11 +5,20 @@ resource "azurerm_storage_account" "storage1" {
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
-}
+  network_rules {
+    default_action = "Deny"
+    bypass         = ["AzureServices"]
 
+    # Restrict to specific public IPs
+    ip_rules = [
+      "203.0.113.10",
+    "198.51.100.0/24"]
+
+  }
+}
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
   storage_account_name  = azurerm_storage_account.storage1.name
   container_access_type = "private"
-
 }
+
