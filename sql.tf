b39677dc13226e9cde7f5a59c12032479d1115fa
James Clease <jclease@jclabs.cloud>
2025-12-07 15:44:00 +0000

    SQL has been deprecated updating code

--- sql.tf
+++ sql.tf
@@ -1,26 +1,22 @@
-resource "azurerm_sql_server" "example" {
-  name                         = "example-sqlserver"
+resource "azurerm_mssql_server" "example" {
+  name                         = "mysqlserver"
   resource_group_name          = azurerm_resource_group.rg.name
   location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
-  administrator_login          = "sqladminuser"
-  administrator_login_password = "SuperSecretPassword123!"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
 }
 
-# Multiple firewall rules, one per IP
-resource "azurerm_sql_firewall_rule" "ip1" {
-  name                = "Allow-IP1"
-  resource_group_name = azurerm_resource_group.example.name
-  server_name         = azurerm_sql_server.example.name
-  start_ip_address    = "203.0.113.10"
-  end_ip_address      = "203.0.113.100"
+resource "azurerm_mssql_firewall_rule" "IP1" {
+  name             = "FirewallRule1"
+  server_id        = azurerm_mssql_server.example.id
+  start_ip_address = "10.0.17.62"
+  end_ip_address   = "10.0.17.89"
 }
 
-resource "azurerm_sql_firewall_rule" "ip2" {
-  name                = "Allow-IP2"
-  resource_group_name = azurerm_resource_group.example.name
-  server_name         = azurerm_sql_server.example.name
-  start_ip_address    = "198.51.100.25"
-  end_ip_address      = "198.51.100.25"
+resource "azurerm_mssql_firewall_rule" "IP2" {
+  name             = "FirewallRule1"
+  server_id        = azurerm_mssql_server.example.id
+  start_ip_address = "10.0.18.62"
+  end_ip_address   = "10.0.18.62"
 }
-
