4216b12728f6bb5d7efc3a7971c02b430e38b638
James Clease <jclease@jclabs.cloud>
2025-12-08 17:37:53 +0000

    Webapp

---
+++ webapp.tf
@@ -0,0 +1,17 @@
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "PremiumV3"
+    size = "P0V3"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "jclabs-app1"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+}
