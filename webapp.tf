6e12d0995b6e07e14484777435e729910118bda0
James Clease <jclease@jclabs.cloud>
2025-12-08 17:46:35 +0000

    web app  IP restrictions

--- webapp.tf
+++ webapp.tf
@@ -14,4 +14,18 @@ resource "azurerm_app_service" "example" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    ip_restriction {
+      ip_address = "86.162.82.32/32"
+      priority   = 100
+      name       = "ETEL FW IP ONLY"
+    }
+    ip_restriction {
+      ip_address = "0.0.0.0/0"
+      priority   = 200
+      name       = "Block all"
+      action     = "Deny"
+    }
+  }
 }
