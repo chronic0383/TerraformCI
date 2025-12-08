c2552391e619788fe08150090ccac9b16a560292
James Clease <jclease@jclabs.cloud>
2025-12-08 16:18:28 +0000

    function app changes in v4 provider

--- Functionapp.tf
+++ Functionapp.tf
@@ -18,11 +18,14 @@ resource "azurerm_app_service_plan" "example" {
   }
 }
 
-resource "azurerm_function_app" "example" {
-  name                       = "test-azure-functions"
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
-  app_service_plan_id        = azurerm_app_service_plan.example.id
+resource "azurerm_windows_function_app" "example" {
+  name                = "example-windows-function-app"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
   storage_account_name       = azurerm_storage_account.example.name
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
+  service_plan_id            = azurerm_service_plan.example.id
+
+  site_config {}
 }
