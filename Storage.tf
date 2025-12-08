f18012da4f9f2d985c5bef89c3568f399d1433e8
James Clease <jclease@jclabs.cloud>
2025-12-08 16:28:52 +0000

    Taken container out

--- Storage.tf
+++ Storage.tf
@@ -16,9 +16,9 @@ resource "azurerm_storage_account" "storage1" {
 
   }
 }
-resource "azurerm_storage_container" "tfstate" {
-  name                  = "tfstate"
-  storage_account_name  = azurerm_storage_account.storage1.name
-  container_access_type = "private"
-}
+//resource "azurerm_storage_container" "tfstate" {
+// name                  = "tfstate"
+//storage_account_name  = azurerm_storage_account.storage1.name
+//container_access_type = "private"
+
 
