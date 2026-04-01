a37239d4949490a28977b61129b4839bb8c9aa7a
James Clease <jclease@jclabs.cloud>
2026-04-01 16:30:55 +0100

    web test removed

--- Availabilitytest.tf
+++ Availabilitytest.tf
@@ -20,42 +20,4 @@ output "app_id" {
   value = azurerm_application_insights.example.app_id
 }
 
-resource "azurerm_application_insights_web_test" "ping_test" {
-  name                    = "availability-test"
-  location                = azurerm_resource_group.example.location
-  resource_group_name     = azurerm_resource_group.example.name
-  application_insights_id = azurerm_application_insights.example.id
-
-  kind = "ping"
-
-  frequency = 300
-  timeout   = 30
-  enabled   = true
-
-  geo_locations = [
-    "emea-nl-ams-azr",
-    "emea-uk-lon-azr"
-  ]
-  configuration = jsonencode({
-    WebTestName            = "availability-test"
-    Description            = "Availability test for your application"
-    Enabled                = true
-    Frequency              = 300
-    Timeout                = 30
-    Url                    = "https://yourapp.com/health"
-    HttpVerb               = "GET"
-    ParseDependentRequests = false
-    FollowRedirects        = true
-    Headers = [
-      {
-        HeaderName  = "Test1"
-        HeaderValue = "my-secret-value"
-      }
-    ]
-    ValidationRules = {
-      ExpectedHttpStatusCode   = 200
-      SSLCertRemainingLifetime = 30
-    }
-  })
-}
 
